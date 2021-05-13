ce1132e0b8f2c7b737f1f0e64a8633ff223e21e3
Christopher Keers <blizzardengle@users.noreply.github.com>
2021-05-13 15:07:57 -0600

    progress on PDF support

--- viewer.js
+++ viewer.js
@@ -22,9 +22,29 @@ function uid() {
 
 function attachPdfListeners( pdfWidget ) {
 
-    var elem = pdfWidget.querySelector('.pdf-toolbar .pdf-download');
+    var elem = pdfWidget.querySelector('.pdf-toolbar .pdf-page-area .pdf-page-up');
+    if ( elem ) {
+        elem.addEventListener( 'click', pdfChangePage );
+    }
+
+    elem = pdfWidget.querySelector('.pdf-toolbar .pdf-page-area .pdf-page-down');
+    if ( elem ) {
+        elem.addEventListener( 'click', pdfChangePage );
+    }
+
+    elem = pdfWidget.querySelector('.pdf-toolbar .pdf-page-area .pdf-page');
+    if ( elem ) {
+        elem.addEventListener( 'keydown', pdfJumpToPage );
+    }
+
+    elem = pdfWidget.querySelector('.pdf-toolbar .pdf-download');
     if ( elem ) {
         elem.addEventListener( 'click', pdfDownload );
+    }
+
+    elem = pdfWidget.querySelector('.pdf-toolbar .pdf-print');
+    if ( elem ) {
+        elem.addEventListener( 'click', pdfPrint );
     }
 
     elem = pdfWidget.querySelector('.pdf-controls .pdf-fit');
@@ -74,11 +94,16 @@ function convertPdfs( elem, password ) {
                 'history': null,
                 'id': id,
                 'linkService': null,
+                'loaded': 0,
                 'loader': null,
                 'pageTotal': 0,
                 'src': elem.dataset.pdf,
                 'viewer': null
-            }
+            };
+
+            pdfObj.eventBus.on( 'pagechanging', function pagechange(evt) { 
+                pdfUpdatePage( evt );
+            } );
 
             var linkService = new pdfjsViewer.PDFLinkService( {
                 'eventBus': pdfObj.eventBus,
@@ -119,10 +144,10 @@ function convertPdfs( elem, password ) {
 
             pdfObj.loader = loadingTask;
 
-            // Can add a fancy loading bar later
-            // loadingTask.onProgress = function (progressData) {
-            //     console.log(progressData.loaded / progressData.total);
-            // };
+            //Can add a fancy loading bar later
+            loadingTask.onProgress = function (progressData) {
+                pdfObj.loaded = progressData.loaded / progressData.total;
+            };
             PDFViewers[ id ] = pdfObj;
 
         } else {
@@ -157,6 +182,7 @@ function convertPdfs( elem, password ) {
                 //console.log( doc );
                 pdfObj.viewer.setDocument( doc );
                 pdfObj.linkService.setDocument( doc );
+                pdfUnlockToolbar( pdfObj );
             },
             // Error function.
             function( exception ) {
@@ -204,6 +230,7 @@ function getPdfContainers( id ) {
 function getPdfControls() {
     var controls = document.createElement('DIV');
     controls.classList.add('pdf-controls');
+    controls.classList.add('pdf-locked');
     var html = '<div class="pdf-button pdf-fit" data-fit="actual">';
     html += '<i class="fas fa-compress pdf-actual pdf-icon"></i>';
     html += '<i class="fas fa-expand pdf-width pdf-icon"></i>';
@@ -219,15 +246,16 @@ function getPdfToolbar() {
 
     var toolbar = document.createElement('DIV');
     toolbar.classList.add('pdf-toolbar');
+    toolbar.classList.add('pdf-locked');
     var html = '<div class="pdf-page-area">';
-    html += '<div class="pdf-button pdf-page-up"><i class="fas fa-arrow-up pdf-icon"></i></div>';
-    html += '<div class="pdf-button pdf-page-down"><i class="fas fa-arrow-down pdf-icon"></i></div>';
-    html += '<input type="number" class="pdf-page" title="Page" value="1" size="4" min="1" autocomplete="off"> <span class="pdf-page-slash">/</span> <span class="pdf-page-total">14</span>';
+    html += '<div class="pdf-button pdf-page-up pdf-disabled"><i class="fas fa-arrow-up pdf-icon"></i></div>';
+    html += '<div class="pdf-button pdf-page-down pdf-disabled"><i class="fas fa-arrow-down pdf-icon"></i></div>';
+    html += '<input type="number" class="pdf-page" title="Page" value="1" size="4" min="1" autocomplete="off"> <span class="pdf-page-slash">/</span> <span class="pdf-page-total">?</span>';
     html += '</div>';
     html += '<div class="pdf-control-area">';
     html += '<div class="pdf-button pdf-download"><i class="fas fa-download pdf-icon"></i></div>';
     html += '<div class="pdf-button pdf-print"><i class="fas fa-print pdf-icon"></i></div>';
-    html += '<div class="pdf-button pdf-bookmark"><i class="fas fa-bookmark pdf-icon"></i></div>';
+    html += '<div class="pdf-button pdf-bookmark pdf-disabled"><i class="fas fa-bookmark pdf-icon"></i></div>';
     html += '</div>';
     toolbar.innerHTML = html;
     return toolbar;
@@ -251,6 +279,139 @@ function getPdfPasswordBox() {
     
 }
 
+function pdfJumpToPage( evt ) {
+    if ( evt ) {
+        if ( evt.key.toUpperCase() == 'ENTER' || evt.keyCode == 13 ) {
+            var widget = this.closest('.pdf-widget');
+            var toolbar = widget.querySelector('.pdf-toolbar');
+            if ( ! toolbar.classList.contains('pdf-disabled') ) {
+                var pdfObj = PDFViewers[ widget.dataset.id ];
+                var page = parseInt( this.value );
+                if ( page < 1 ) {
+                    page = 1;
+                } else if ( page > pdfObj.pageTotal ) {
+                    page = pdfObj.pageTotal
+                }
+                pdfObj.linkService.goToPage( page );
+            }
+        }
+    }
+}
+
+function pdfChangePage() {
+    if ( ! this.classList.contains('pdf-disabled') ) {
+        var widget = this.closest('.pdf-widget');
+        var pdfObj = PDFViewers[ widget.dataset.id ];
+        var current = parseInt( widget.querySelector('.pdf-page-area .pdf-page').value );
+        if ( this.classList.contains('pdf-page-up') ) {
+            // Go back (up) a page.
+            current--;
+            if ( current > 0 ) {
+                pdfObj.linkService.goToPage( current );
+            }
+        } else {
+            // Go forward (down) a page.
+            current++;
+            if ( current <= pdfObj.pageTotal ) {
+                pdfObj.linkService.goToPage( current );
+            }
+        }
+    }
+}
+
+function pdfUpdatePage( evt ) {
+    var widget = evt.source.container.closest('.pdf-widget');
+    var up = widget.querySelector('.pdf-page-area .pdf-page-up');
+    var down = widget.querySelector('.pdf-page-area .pdf-page-down');
+    var pageTotal = widget.querySelector('.pdf-toolbar .pdf-page-area .pdf-page-total');
+    var total = parseInt( pageTotal.innerHTML );
+    var pageNum = widget.querySelector('.pdf-toolbar .pdf-page-area .pdf-page');
+    pageNum.value = evt.pageNumber;
+
+    if ( total == 1 ) {
+        up.classList.add('pdf-disabled');
+        down.classList.add('pdf-disabled');
+    } else if ( evt.pageNumber == 1 ) {
+        up.classList.add('pdf-disabled');
+        down.classList.remove('pdf-disabled');
+    } else if ( evt.pageNumber == total ) {
+        up.classList.remove('pdf-disabled');
+        down.classList.add('pdf-disabled');
+    } else {
+        up.classList.remove('pdf-disabled');
+        down.classList.remove('pdf-disabled');
+    }
+
+}
+
+function pdfCreateBookmarks( bookmarks ) {
+    var html = '<ul>';
+    for ( var i = 0; i < bookmarks.length; i++ ) {
+        html += '<li><div class="pdf-bookmark-link" data-dest=\'' + JSON.stringify( bookmarks[i].dest ) + '\'>' + bookmarks[i].title + '</div>';
+        if ( bookmarks[i].items.length > 0 ) {
+            html += pdfCreateBookmarks( bookmarks[i].items );
+        }
+        html += '</li>';
+    }
+    return html + '</ul>';
+}
+
+function pdfGoToBookmark() {
+    var widget = this.closest('.pdf-widget');
+    var pdfObj = PDFViewers[ widget.dataset.id ];
+    var dest = JSON.parse( this.dataset.dest );
+    if ( dest ) {
+        pdfObj.linkService.goToDestination( dest );
+    }
+}
+
+function pdfUnlockToolbar( pdfObj ) {
+
+    var widget = document.querySelector( '[data-id="' + pdfObj.id + '"]' );
+    var toolbar = widget.querySelector('.pdf-toolbar');
+    var total = toolbar.querySelector('.pdf-page-area .pdf-page-total');
+    var controls = widget.querySelector('.pdf-controls');
+    toolbar.classList.remove('pdf-locked');
+    controls.classList.remove('pdf-locked');
+    if ( pdfObj.pageTotal > 1 ) {
+        total.innerHTML = pdfObj.pageTotal;
+    } else {
+        total.innerHTML = 1;
+        total.parentElement.style.display = 'none';
+    }
+
+    // Dispatch a fake page change event to update the GUI.
+    pdfObj.eventBus.dispatch( 'pagechanging', {
+        'source': pdfObj.viewer,
+        'pageNumber': 1,
+        'pageLabel': null,
+        'previous': 0
+    } );
+
+    // pdfObj.document.getOutline().then(
+    //     function( response ) {
+    //         if ( response && response.length > 0 ) {
+    //             var html = pdfCreateBookmarks( response );
+    //             var bookmarks = document.createElement('DIV');
+    //             bookmarks.classList.add('pdf-bookmark-links');
+    //             bookmarks.innerHTML = html;
+    //             widget.appendChild( bookmarks );
+    //             var links = bookmarks.querySelectorAll('.pdf-bookmark-link');
+    //             links.forEach( function( link ) {
+    //                 link.addEventListener( 'click', pdfGoToBookmark );
+    //             } );
+    //         } else {
+    //             var bookmark = toolbar.querySelector('.pdf-control-area .pdf-bookmark');
+    //             bookmark.style.display = 'none';
+    //         }
+    //     },
+    //     function( error ) {
+
+    //     }
+    // );
+    
+}
+
 function pdfUnlock() {
     var password = this.previousSibling;
     if ( password ) {
@@ -261,8 +422,10 @@ function pdfUnlock() {
 }
 
 function pdfDownload() {
-    var id = this.closest('.pdf-widget').dataset.id;
-    if ( id ) {
+    var widget = this.closest('.pdf-widget');
+    var toolbar = widget.querySelector('.pdf-toolbar');
+    var id = widget.dataset.id;
+    if ( id && ! toolbar.classList.contains('pdf-locked') ){
         var pdfObj = PDFViewers[ id ];
         // https://github.com/mozilla/pdf.js/pull/12137/files
         pdfObj.document.saveDocument( pdfObj.document.annotationStorage ).then(
@@ -271,6 +434,31 @@ function pdfDownload() {
                 var url = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
                 var blob = new Blob( [data], { type: "application/pdf" } );
                 pdfObj.download.download( blob, url, 'test.pdf' );
+            },
+            function err( e ) {
+                console.log( e );
+            }
+        );
+    }
+}
+
+function pdfPrint() {
+    var widget = this.closest('.pdf-widget');
+    var toolbar = widget.querySelector('.pdf-toolbar');
+    var id = widget.dataset.id;
+    if ( id && ! toolbar.classList.contains('pdf-locked') ) {
+        var pdfObj = PDFViewers[ id ];
+        // https://github.com/mozilla/pdf.js/pull/12137/files
+        pdfObj.document.saveDocument( pdfObj.document.annotationStorage ).then(
+            function suc( data ) {
+                var getUrl = window.location;
+                var url = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+                var blob = new Blob( [data], { type: "application/pdf" } );
+                var url = URL.createObjectURL( blob );
+                var a = document.createElement("A");
+                a.target = '_blank';
+                a.href = url;
+                a.click();
             },
             function err( e ) {
                 console.log( e );
